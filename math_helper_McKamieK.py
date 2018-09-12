d26e8667b28e55351329ff90b95066e8fa277aae
Kaylamck202 <kaylamck202@gmail.com>
2018-09-12 14:06:44 -0500

    change made

--- math_helper_McKamieK.py
+++ math_helper_McKamieK.py
@@ -3,3 +3,20 @@
     area of trapezoid,area of circle,
     midpoint.
 '''
+
+import math
+
+def distance(x1,x2,y1,y2):
+    
+    
+    
+    
+    
+def area_trapezoid(b1,b2,h):
+    
+    
+def area_circ(r):
+    
+def midpoint(x1,x2,y1,y2):
+    
+def pythag_thrm(a,b,c)
