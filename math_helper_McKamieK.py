820e7af3d41d48af8adff5ac391032ddc2fd70ff
Kaylamck202 <kaylamck202@gmail.com>
2018-09-13 14:11:06 -0500

    work on dist

--- math_helper_McKamieK.py
+++ math_helper_McKamieK.py
@@ -7,17 +7,24 @@
 
 import math
 
-def distance(x1,x2,y1,y2):
-    
-    
+def dist(x1,x2,y1,y2):
+    pass
+#    '''should give the distance between two points
+#'''
+#    a= x2-x1
+#    b= y2-y1
+#    c= a*a
+#    d= b*b
     
     
     
 def area_trapezoid(b1,b2,h):
-    
+    ''
+    pass
     
 def area_circ(r):
-    
+    pass
 def midpoint(x1,x2,y1,y2):
-    
-def pythag_thrm(a,b,c)
+    pass
+def pythag_thrm(a,b,c):
+    pass
