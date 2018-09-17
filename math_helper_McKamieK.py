5514190e77d1cd66d7814db62c7d8b5571739a24
Kaylamck202 <kaylamck202@gmail.com>
2018-09-17 14:05:14 -0500

    dist works

--- math_helper_McKamieK.py
+++ math_helper_McKamieK.py
@@ -7,19 +7,22 @@
 
 import math
 
-choice=input("Choose a formula from these options:(1)distance, (2)area of a trapezoid, (3)area of a circle, (4)midpoint, or (5)pythag theorem:")
-if choice == 1:
-    x1= input("insert your first x coordinate:")
-    def distance(x1,x2,y1,y2):
-            '''should give the distance between two points
-            '''
-            a=x2-x1
-            b= y2-y1
-            c= a*a
-            d= b*b
-            e=c + d
-            f=math.sqrt(e)
-            return f
+#choice=input("Choose a formula from these options:(1)distance, (2)area of a trapezoid, (3)area of a circle, (4)midpoint, or (5)pythag theorem:")
+#if choice == 1:
+#    x1= input("insert your first x coordinate:")
+#    x2 = input("insert your second x coordinate:")
+#    y1 = input("insert your first y coordinate:")
+#    y2 = input("insert your second y coordinate:")
+def distance(x1,x2,y1,y2):
+        '''should give the distance between two points
+        '''
+        a=x2-x1
+        b= y2-y1
+        c= a*a
+        d= b*b
+        e=c + d
+        f=math.sqrt(e)
+        return f
         
     
 def area_trapezoid(b1,b2,h):
