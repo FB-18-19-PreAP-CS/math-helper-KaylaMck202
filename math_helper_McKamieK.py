27c7b537d0534b88b6a574bf2347dc8759f48682
Kaylamck202 <kaylamck202@gmail.com>
2018-09-20 13:47:25 -0500

    run_dist added

--- math_helper_McKamieK.py
+++ math_helper_McKamieK.py
@@ -6,8 +6,10 @@
 '''
 
 import math
+import doctest
+#doctest.testmod()
 
-#choice=input("Choose a formula from these options:(1)distance, (2)area of a trapezoid, (3)area of a circle, (4)midpoint, or (5)pythag theorem:")
+#
 #if choice == 1:
 #    x1= input("insert your first x coordinate:")
 #    x2 = input("insert your second x coordinate:")
@@ -21,6 +23,10 @@ def distance(x1,x2,y1,y2):
         5
         >>> distance(10,15,6,12)
         7.810249675906654
+        >>> distance(1,0.5,2,7.6)
+        5.622277118748239
+        >>> distance(-7,9,5,6.5)
+        16.0701586799882
         '''
         a=x2-x1
         b= y2-y1
@@ -39,6 +45,10 @@ def area_trapezoid(b1,b2,h):
     75
     >>>area_trapezoid(11,2,3)
     19.5
+    >>>area_trapezoid(-4,6,12)
+    12
+    >>>area_trapezoid(11,0.5,7.6)
+    43.7
     '''
     b3 = b1+b2
     a= b3*h
@@ -53,6 +63,10 @@ def area_circ(r):
     12.56
     >>>area_circ(11)
     379.94
+    >>>area_circ(1.05)
+    3.46185
+    >>>area_circ(-8.9)
+    248.7194
     '''
     r2= r**2
     r3 = r2*3.14
@@ -66,6 +80,10 @@ def midpoint(x1,x2,y1,y2):
     (8.5,6)
     >>>midpoint(18,20,21,6)
     (19,13.5)
+    >>>midpoint(11,0.5,6.5,8)
+    (5.75, 7.25)
+    >>>midpoint(16,2.6,100,-8)
+    (9.3,46)
     '''
     x3= x1+x2
     y3= y1+y2
@@ -81,16 +99,26 @@ def pythag_thrm(a,b):
     c=8.602325267042627
     >>>pythag_thrm(-6,-8)
     c=10
+    >>>pythag_thrm(4,7)
+    c=8.062257748
+    >>>pythag_thrm(-3.5,7.2)
+    c=8.005623024
     '''
     a2=a**2
     b2=b**2
     a3=a2+b2
     a4= math.sqrt(a3)
     print(f"c= {a4}")
-    
-
-#def main():
-#    
-#if __name__ == "__main__":
-#    main()
+def run_dist():
+    x1= int(input("Enter your first x coordinate: "))
+    x2= int(input("Enter your second x coordinate: "))
+    y1= int(input("Enter your first y coordinate: "))
+    y2= int(input("Enter your second y coordinate: "))
+    distance(x1,x2,y1,y2)
+def main():
+    choice=input("Choose a formula from these options:(1)distance, (2)area of a trapezoid, (3)area of a circle, (4)midpoint, or (5)pythag theorem:")
+    if choice == "1":
+        run_dist()
+if __name__ == "__main__":
+    main()
     
