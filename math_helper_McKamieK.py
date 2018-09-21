b47b91896212591fb8bb5265460ed088a02c0d96
kaylamck202 <kaylamck202@gmail.com>
2018-09-20 20:24:56 -0500

    doctest added

--- math_helper_McKamieK.py
+++ math_helper_McKamieK.py
@@ -17,14 +17,19 @@ import doctest
 #    y2 = input("insert your second y coordinate:")
 def distance(x1,x2,y1,y2):
         '''returns the distance between two points
+
         >>> distance(4,2,-5,6)
         11.18033988749895
+        
         >>> distance(5,9,4,7)
-        5
+        5.0
+        
         >>> distance(10,15,6,12)
         7.810249675906654
+        
         >>> distance(1,0.5,2,7.6)
         5.622277118748239
+        
         >>> distance(-7,9,5,6.5)
         16.0701586799882
         '''
@@ -39,16 +44,21 @@ def distance(x1,x2,y1,y2):
     
 def area_trapezoid(b1,b2,h):
     '''returns the area of a trapezoid
-    >>>area_trapezoid(4,6,5)
-    25
-    >>>area_trapezoid(7,8,10)
-    75
-    >>>area_trapezoid(11,2,3)
-    19.5
-    >>>area_trapezoid(-4,6,12)
-    12
-    >>>area_trapezoid(11,0.5,7.6)
-    43.7
+
+     >>> area_trapezoid(4,6,5)
+     25.0
+     
+     >>> area_trapezoid(7,8,10)
+     75.0
+     
+     >>> area_trapezoid(11,2,3)
+     19.5
+     
+     >>> area_trapezoid(-4,6,12)
+     12.0
+     
+     >>> area_trapezoid(11,0.5,7.6)
+     43.7
     '''
     b3 = b1+b2
     a= b3*h
@@ -57,33 +67,43 @@ def area_trapezoid(b1,b2,h):
     
 def area_circ(r):
     '''returns the area of a circle
-    >>>area_circ(6)
+
+    >>> area_circ(6)
     113.04
-    >>>area_circ(-2)
+    
+    >>> area_circ(-2)
     12.56
-    >>>area_circ(11)
+    
+    >>> area_circ(11)
     379.94
-    >>>area_circ(1.05)
+    
+    >>> area_circ(1.05) 
     3.46185
-    >>>area_circ(-8.9)
+    
+    >>> area_circ(-8.9)
     248.7194
     '''
     r2= r**2
     r3 = r2*3.14
-    return r3
+    return(round(r3,5))
     
 def midpoint(x1,x2,y1,y2):
     '''returns the midpoint of a line
-    >>>midpoint(4,10,6,8)
-    (7,7)
-    >>>midpoint(6,11,16,-4)
-    (8.5,6)
-    >>>midpoint(18,20,21,6)
-    (19,13.5)
-    >>>midpoint(11,0.5,6.5,8)
+
+    >>> midpoint(4,10,6,8)
+    (7.0, 7.0)
+    
+    >>> midpoint(6,11,16,-4)
+    (8.5, 6.0)
+    
+    >>> midpoint(18,20,21,6)
+    (19.0, 13.5)
+    
+    >>> midpoint(11,0.5,6.5,8)
     (5.75, 7.25)
-    >>>midpoint(16,2.6,100,-8)
-    (9.3,46)
+    
+    >>> midpoint(16,2.6,100,-8)
+    (9.3, 46.0)
     '''
     x3= x1+x2
     y3= y1+y2
@@ -94,16 +114,21 @@ def midpoint(x1,x2,y1,y2):
     
 def pythag_thrm(a,b):
     '''returns the length, or size, of the side of a triangle that is missing
-    >>>pythag_thrm(10,2)
-    c=10.19803902718557
-    >>>pythag_thrm(5,7)
-    c=8.602325267042627
-    >>>pythag_thrm(-6,-8)
-    c=10
-    >>>pythag_thrm(4,7)
-    c=8.062257748
-    >>>pythag_thrm(-3.5,7.2)
-    c=8.005623024
+
+    >>> pythag_thrm(10,2)
+    10.19803902718557
+    
+    >>> pythag_thrm(5,7)
+    8.602325267042627
+    
+    >>> pythag_thrm(-6,-8)
+    10.0
+    
+    >>> pythag_thrm(4,7)
+    8.062257748
+    
+    >>> pythag_thrm(-3.5,7.2)
+    8.005623024
     '''
     a2=a**2
     b2=b**2
@@ -160,5 +185,7 @@ def main():
             run_pythag()
     
 if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
     main()
     
